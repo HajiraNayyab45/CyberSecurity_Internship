fd3439b2d15df7f23f32c9f75dea708e8cdcb423
Hajira Nayyab <hajra.bsse4515@iiu.edu.pk>
2025-05-24 13:22:39 +0500

    Update Basic_network_sniffer.py

--- Network_Sniffer/Basic_network_sniffer.py
+++ Network_Sniffer/Basic_network_sniffer.py
@@ -1,1 +1,8 @@
+from scapy.all import sniff, hexdump
 
+def packet_callback(packet):
+     print(packet.show())  # Show detailed packet information
+     print(hexdump(packet))  # Display raw packet data in hex format
+
+print("Sniffer running...")
+sniff(prn=packet_callback, store=False)
